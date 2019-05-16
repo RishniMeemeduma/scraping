b7ce3a44ef34ea90beccf883e82bb30d58da4b90
Rishni <ashameemeduma@gmail.com>
2019-05-16 20:57:59 +0530

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$html = file_get_contents('https://pokemondb.net/evolution');
+
+$doc = new DOMDocument();
+
+libxml_use_internal_errors(TRUE);
+
+if(!empty($html)){
+
+	$doc->loadHTML($html);
+	libxml_clear_errors();
+
+	$xpath = new DOMXPATH($doc);
+
+	$row = $xpath ->query('//h2[@id]');
+
+	echo $row->nodeValue ."<br>";
+}
