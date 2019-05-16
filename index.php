e71ed726e759d251bf4d9a7b5cf822e6b82928d2
Rishni <ashameemeduma@gmail.com>
2019-05-16 22:19:14 +0530

    1

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(!empty($html)){ //if any html is actually returned
 
 	//get all the h2's with an id
 	$array1= array();
-	$pokemon_row = $pokemon_xpath->query('//h3[@class="media__content"]');
+	$pokemon_row = $pokemon_xpath->query('//div[@class="media__content"]');
 
 	if($pokemon_row->length>0){
 
