16c92633954460c55c6693d452ff4d4f0496d0fe
Rishni <ashameemeduma@gmail.com>
2019-05-17 12:33:16 +0530

    1

--- index.php
+++ index.php
@@ -7,25 +7,25 @@ $html = file_get_contents('https://www.bbc.com/'); //get the html returned from 
 
 if(!empty($html)){ //if any html is actually returned
 
-	$pokemon_doc->loadHTML($html);
+	// $pokemon_doc->loadHTML($html);
 	// libxml_clear_errors(); //remove errors for yucky html
 	
 	// $pokemon_xpath = new DOMXPath($pokemon_doc);
 
 	//get all the h2's with an id
 	$array1= array();
-	$pokemon_row = $pokemon_xpath->query('//li[@class="media-list__item"]');
+	$pokemon_row = $pokemon_xpath->query('//div[@class="media__content"]');
 
 	if($pokemon_row->length>0){
 
 		foreach ($pokemon_row as $value) {
 			
 			for($i = 0;$i<count($array1) ;$i++){
-			//$image =$pokemon_xpath->query('//div[@class="media__image"]',$value)->item($i)->nodeValue;
+				
 			$title = $pokemon_xpath->query('//h3[@class="media__title"]',$value)->item($i)->nodeValue;
 			$content =$pokemon_xpath->query('//p[@class="media__summary"]',$value)->item($i)->nodeValue;
 		}
-			$array1[]= array('img'=>$image,'title'=>$title,'content'=>$content);
+			$array1[]= array('title'=>$title,'content'=>$content);
 			
 		}
 	}
