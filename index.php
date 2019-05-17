865c9a60e8de00e901209368943a4987a13cc95e
Rishni <ashameemeduma@gmail.com>
2019-05-17 12:34:59 +0530

    1

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 $html = file_get_contents('https://www.bbc.com/'); //get the html returned from the following url
 
-// $pokemon_doc = new DOMDocument();
+$pokemon_doc = new DOMDocument();
 
-// libxml_use_internal_errors(TRUE); //disable libxml errors
+libxml_use_internal_errors(TRUE); //disable libxml errors
 
 if(!empty($html)){ //if any html is actually returned
 
-	// $pokemon_doc->loadHTML($html);
-	// libxml_clear_errors(); //remove errors for yucky html
+	$pokemon_doc->loadHTML($html);
+	libxml_clear_errors(); //remove errors for yucky html
 	
-	// $pokemon_xpath = new DOMXPath($pokemon_doc);
+	$pokemon_xpath = new DOMXPath($pokemon_doc);
 
 	//get all the h2's with an id
 	$array1= array();
-	$pokemon_row = $pokemon_xpath->query('//div[@class="media__content"]');
+	$pokemon_row = $pokemon_xpath->query('//li[@class="media-list__item"]');
 
 	if($pokemon_row->length>0){
 
 		foreach ($pokemon_row as $value) {
 			
 			for($i = 0;$i<count($array1) ;$i++){
-				
+
 			$title = $pokemon_xpath->query('//h3[@class="media__title"]',$value)->item($i)->nodeValue;
 			$content =$pokemon_xpath->query('//p[@class="media__summary"]',$value)->item($i)->nodeValue;
 		}
