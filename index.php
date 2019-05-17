c5f1cad5e6657a8dccb3f1f2ebb691caa873a5d9
Rishni <ashameemeduma@gmail.com>
2019-05-17 13:30:34 +0530

    1

--- index.php
+++ index.php
@@ -14,18 +14,19 @@ if(!empty($html)){ //if any html is actually returned
 
 	//get all the h2's with an id
 	$array1= array();
-	$pokemon_row = $pokemon_xpath->query('//div[@class="media__content"]');
+	$pokemon_row = $pokemon_xpath->query('//div[@class="media media--overlay block-link"]');
+	
 
 	if($pokemon_row->length>0){
 
 		foreach ($pokemon_row as $value) {
 			
 			for($i = 0;$i<count($array1) ;$i++){
-
+				$image = $pokemon_xpath->query('//img',$value)->item($i)->nodeValue;
 			$title = $pokemon_xpath->query('//h3[@class="media__title"]',$value)->item($i)->nodeValue;
 			$content =$pokemon_xpath->query('//p[@class="media__summary"]',$value)->item($i)->nodeValue;
 		}
-			$array1[]= array('title'=>$title,'content'=>$content);
+			$array1[]= array('image'=>$image,'title'=>$title,'content'=>$content);
 			
 		}
 	}
